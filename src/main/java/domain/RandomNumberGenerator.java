f4970c11d5f7166889d2501b5a83c25e9d79ac10
jaeju.jang <jjj0611@gmail.com>
2020-01-09 15:54:38 +0900

    Complete feature #5.
    	- make RandomNumberGenerator with static method

---
+++ src/main/java/domain/RandomNumberGenerator.java
@@ -0,0 +1,13 @@
+package domain;
+
+import java.util.Random;
+
+public class RandomNumberGenerator {
+
+    private static final Random random = new Random();
+    private static final int NUMBER_BOUND = 10;
+
+    public static int generateNumber() {
+        return random.nextInt(NUMBER_BOUND);
+    }
+}
---
+++ src/test/java/domain/RandomNumberGeneratorTest.java
@@ -0,0 +1,14 @@
+package domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class RandomNumberGeneratorTest {
+
+    @Test
+    void generateRnadomNumber() {
+        int number = RandomNumberGenerator.generateNumber();
+        assertTrue(0 <= number && number <= 9);
+    }
+}
