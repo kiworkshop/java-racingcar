871667c7deace8117305f3db25ec8a2ed15670d6
harris <harrisleesh@gmail.com>
2020-01-11 01:35:47 +0900

    Refactoring, test 구현

--- src/main/java/RacingCar.java
+++ src/main/java/RacingCar.java
@@ -1,4 +1,21 @@
 public class RacingCar {
     private String name;
     private int distance;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+
+    public RacingCar(String name) {
+        this.name = name;
+        this.distance = 0;
+    }
 }
---
+++ src/test/java/TestRacingGame.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+public class TestRacingGame {
+
+    @Test
+    public void 생성자테스트(){
+
+    }
+}
