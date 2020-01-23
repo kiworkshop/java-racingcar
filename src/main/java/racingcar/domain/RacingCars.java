cdb826157467f690b867691cebb2f1e743be2bcf
YOO TS <yrns1213@naver.com>
2020-01-23 21:03:52 +0900

    test: RacingCarsTest 추가
    - 중복된 car가 있는 경우 illegalArgumentException을 throw하도록
    RacingCars를 수정

--- src/main/java/racingcar/domain/RacingCars.java
+++ src/main/java/racingcar/domain/RacingCars.java
@@ -1,11 +1,33 @@
 package racingcar.domain;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class RacingCars {
 
     List<RacingCar> cars = new ArrayList<>();
 
+    public RacingCars(List<RacingCar> cars) {
+        if (hasDuplicatedNamesIn(cars)) {
+            throw new IllegalArgumentException();
+        }
+        this.cars = cars;
+    }
 
+    public boolean hasDuplicatedNamesIn(List<RacingCar> cars) {
+        Set<RacingCar> nameSet = new HashSet<>(cars);
+        return cars.size() != nameSet.size();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RacingCars that = (RacingCars) o;
+        return Objects.equals(cars, that.cars);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cars);
+    }
 }
---
+++ src/test/java/racingcar/domain/RacingCarTest.java
@@ -0,0 +1,15 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.Test;
+
+public class RacingCarTest {
+    @Test
+    public void 차의_이름이_다섯자_이내인지_체크() {
+        // given
+
+        // when
+
+        // then
+
+    }
+}
