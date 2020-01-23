c87d66c0ddb4acba153237f80c94c5e2282abfa4
YOO TS <yrns1213@naver.com>
2020-01-23 18:13:55 +0900

    feat: RacingCarService에 RacingCars를 만드는 로직 추가
    - delimeter를 기준으로 자동차 이름을 분리하여 RacingCar의 List를 생성
    - 이 List를 RacingCars의 생성자에 전달

--- src/main/java/racingcar/service/RacingCarService.java
+++ src/main/java/racingcar/service/RacingCarService.java
@@ -1,11 +1,30 @@
 package racingcar.service;
 
+import racingcar.domain.RacingCar;
 import racingcar.domain.RacingCars;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class RacingCarService {
 
-    public RacingCars produceCarsWith(String carNames) {
+    private static final String CAR_NAME_DELIMITER = ",";
 
-        return new RacingCars();
+    public RacingCars produceCarsWith(String carNames) {
+        String[] parsedCarNames = parseCarNames(carNames);
+        List<RacingCar> cars = produceCarsWith(parsedCarNames);
+        return new RacingCars(cars);
+    }
+
+    public List<RacingCar> produceCarsWith(String[] parsedCarNames) {
+        return Arrays.stream(parsedCarNames)
+                .map(String::trim)
+                .map(RacingCar::new)
+                .collect(Collectors.toList());
+    }
+
+    private String[] parseCarNames(String carNames) {
+        return carNames.split(CAR_NAME_DELIMITER);
     }
 }
